23443354256795eb399903480c3018c97eeb25c7
Craig <Craig@dimitri.home>
2013-01-16 21:33:21 +0000

    Initial Commit

---
+++ SPOJ/basicPrimalityTest.cpp
@@ -0,0 +1,20 @@
+// working.cpp by Craig
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int numTest = 101;
+	bool primeFlag = true;
+
+	cout << "Number to test for primality: " << numTest << "\n";
+	cout << "Test numbers: \n";
+	for (int n = 2 ; n <= sqrt(numTest) ; n++) {
+		if (numTest % n == 0) primeFlag = false;
+	}
+
+	primeFlag == true ? cout << numTest << " is prime\n" : cout << numTest << " is not prime\n" ;
+	return 0;
+}
